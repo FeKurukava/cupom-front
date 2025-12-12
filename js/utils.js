63abbb1eff1b6e93c66fdd4fcbb9e7681f9ae1f6
FeKurukava <maria.kurukava@hotmail.com>
2025-12-12 15:02:41 -0300

    Mascara CPF e CNPJ

--- js/utils.js
+++ js/utils.js
@@ -20,3 +20,23 @@ function limparDocumento(cpfCnpj) {
     if (cpfCnpj == null) return null;
     return cpfCnpj.replace(/[^0-9]/g, '');
 }
+
+export const maskCPF_CNPJ = (value) => {
+    if (!value) return '';
+    const numbers = String(value).replace(/\D/g, '');
+
+    if (numbers.length <= 11) {
+        return numbers
+            .replace(/^(\d{3})(\d)/, '$1.$2')
+            .replace(/^(\d{3})\.(\d{3})(\d)/, '$1.$2.$3')
+            .replace(/^(\d{3})\.(\d{3})\.(\d{3})(\d)/, '$1.$2.$3-$4')
+            .slice(0, 14); // CPF: 000.000.000-00
+    }
+
+    return numbers
+        .replace(/^(\d{2})(\d)/, '$1.$2')
+        .replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3')
+        .replace(/^(\d{2})\.(\d{3})\.(\d{3})(\d)/, '$1.$2.$3/$4')
+        .replace(/^(\d{2})\.(\d{3})\.(\d{3})\/(\d{4})(\d)/, '$1.$2.$3/$4-$5')
+        .slice(0, 18); // CNPJ: 00.000.000/0000-00
+};
